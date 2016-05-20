e5b428b85f72ac109a846e2b92aa612f6c30204d
Chris Hill-Scott <me@quis.cc>
2016-05-20 15:34:13 +0100

    STASH2

--- app/main/views/tour.py
+++ app/main/views/tour.py
@@ -3,18 +3,10 @@ from flask import render_template
 from app.main import main
 
 
-headings = [
-    'Start with a template',
-    'Add recipients',
-    'Send your messages',
-]
-
-
 @main.route("/tour/<int:page>")
 def tour(page):
     return render_template(
         'views/tour/{}.html'.format(page),
         current_page=page,
-        next_page=(page + 1),
-        heading=headings[page - 1]
+        next_page=(page + 1)
     )
