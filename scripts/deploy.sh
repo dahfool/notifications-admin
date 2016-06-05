908f809ddb42852f001297e2eee8aad3327a8ad4
Chris Hill-Scott <me@quis.cc>
2016-06-05 08:15:47 +0100

    use var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,6 +19,8 @@ LCYAN='\033[01;36m'
 WHITE='\033[01;37m'
 
 REMOTE="$(git config --get remote.origin.url)"
+STAGING_TAG="staging"
+LIVE_TAG="live"
 
 echo ""
 echo "═════════════════════════════════════════════════════════════════════════════════════"
@@ -27,31 +29,34 @@ git fetch
 echo "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
 echo -e  "${PURPLE}local    $(git log -n1 --oneline master)${RESTORE}"
 echo -e "${LPURPLE}preview  $(git log -n1 --oneline origin/master)${RESTORE}"
-echo -e   "${LBLUE}staging  $(git log -n1 --oneline origin/staging)${RESTORE}"
-echo -e  "${LGREEN}live     $(git log -n1 --oneline origin/live)${RESTORE}"
+echo -e   "${LBLUE}staging  $(git log -n1 --oneline origin/${STAGING_TAG})${RESTORE}"
+echo -e  "${LGREEN}live     $(git log -n1 --oneline origin/${LIVE_TAG})${RESTORE}"
 echo ""
 echo "═════════════════════════════════════════════════════════════════════════════════════"
-if [ "$1" == "staging" ]; then
-    echo -e "Tagging ${LPURPLE}preview${RESTORE} for deployment to ${LBLUE}staging${RESTORE}"
-    git tag -f staging origin/master
-elif [ "$1" == "live" ]; then
-    echo -e "Tagging ${LBLUE}staging${RESTORE} for deployment to ${LGREEN}live${RESTORE}"
-    git tag -f live origin/staging
+if [ "$1" == "" ]; then
+    echo -e "${RED}No environment specified, try deploy.sh ${STAGING_TAG}${RESTORE}"
+    exit 1
+elif [ "$1" == "$STAGING_TAG" ]; then
+    echo -e "Tagging ${LPURPLE}preview${RESTORE} for deployment to ${LBLUE}${STAGING_TAG}${RESTORE}"
+    git tag -f $STAGING_TAG origin/master
+elif [ "$1" == "$LIVE_TAG" ]; then
+    echo -e "Tagging ${LBLUE}${STAGING_TAG}${RESTORE} for deployment to ${LGREEN}${LIVE_TAG}${RESTORE}"
+    git tag -f $LIVE_TAG origin/staging
 else
     echo -e "${RED}“$1” isn’t a something that can be deployed to${RESTORE}"
-    exit 0
+    exit 1
 fi
 echo "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
 echo "Done…"
 echo ""
 echo "═════════════════════════════════════════════════════════════════════════════════════"
 if [ "$1" == "staging" ]; then
-    echo -e "Pushing ${LBLUE}staging${RESTORE} tag to ${REMOTE}"
+    echo -e "Pushing ${LBLUE}${STAGING_TAG}${RESTORE} tag to ${REMOTE}"
     echo "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
-    # git push origin staging
+    # git push origin $STAGING_TAG
 elif [ "$1" == "live" ]; then
-    echo -e "Pushing ${LGREEN}live${RESTORE} tag to ${REMOTE}"
+    echo -e "Pushing ${LGREEN}${LIVE_TAG}${RESTORE} tag to ${REMOTE}"
     echo "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――"
-    # git push origin live
+    # git push origin $LIVE_TAG
 fi
 echo "Done"
